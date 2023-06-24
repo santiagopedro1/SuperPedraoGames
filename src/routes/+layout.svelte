b66ead269b1c40ccef48f163350cb3498dd0e135
Pedro Santiago <pedrossantiago1@gmail.com>
2023-06-24 09:31:03 -0300

    pagina inicial

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <div class="h-screen">
-	<header class="flex justify-center px-3 py-2 bg-purple-900 text-white relative">
-		{#if $page.route.id != '/'}
+	{#if $page.route.id != '/'}
+		<header class="flex justify-center px-3 py-2 bg-purple-900 text-white relative">
 			<a
 				href="/"
 				class="w-fit p-2 absolute left-4 top-2"
@@ -16,10 +16,9 @@
 					alt="Back"
 				/>
 			</a>
-		{/if}
-
-		<h1 class="text-xl py-2 font-bold">SUPER PEDRÃO GAMES</h1>
-	</header>
+			<h1 class="text-xl py-2 font-bold">SUPER PEDRÃO GAMES</h1>
+		</header>
+	{/if}
 
 	<main>
 		<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col gap-3 px-6 py-4">
+	<h1 class="text-center font-black text-2xl">Super Pedrão Games</h1>
+
+	<h2 class="text-lg">Jogos:</h2>
+
+	<ul class="ml-4">
+		<li><a href="pooperscooper">Pooper Scooper</a></li>
+	</ul>
+</div>
